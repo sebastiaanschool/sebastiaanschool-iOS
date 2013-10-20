3a999bbaba99e3b6eba578cf6748f72b19c899be
Jeroen Leenarts <jeroen.leenarts@gmail.com>
2013-10-21 00:14:16 +0200

    Added unit testing.

---
+++ SebastiaanSchool/SebastiaanSchool Tests/SebastiaanSchool_Tests.m
@@ -0,0 +1,34 @@
+//
+//  SebastiaanSchool_Tests.m
+//  SebastiaanSchool Tests
+//
+//  Created by Jeroen Leenarts on 21-10-13.
+//
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SebastiaanSchool_Tests : XCTestCase
+
+@end
+
+@implementation SebastiaanSchool_Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
