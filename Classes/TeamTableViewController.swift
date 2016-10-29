1a497d1d7485b79fdf844ec69a7f6168c32a3db5
Jeroen Leenarts <jeroen.leenarts@gmail.com>
2016-10-29 22:20:04 +0200

    Add mail feature.

--- Classes/TeamTableViewController.swift
+++ Classes/TeamTableViewController.swift
@@ -8,8 +8,9 @@
 
 import UIKit
 import RealmSwift
+import MessageUI
 
-class TeamTableViewController: UITableViewController {
+class TeamTableViewController: UITableViewController, MFMailComposeViewControllerDelegate {
     let realm = try! Realm()
     
     var contactItemsArray = try! Realm().objects(ContactItem.self).sorted(byProperty: "order", ascending: false)
@@ -73,6 +74,66 @@ class TeamTableViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return contactItemsArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        let selectedItem = contactItemsArray[indexPath.row]
+        
+        let displayName = selectedItem.displayName
+        guard let emailRecipient = selectedItem.email else {
+            let title = NSLocalizedString("Unknown email addres", comment: "Unknown email addres")
+            let message = String.localizedStringWithFormat("No email address of %@ on file.", displayName)
+            
+            let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+            let okAction = UIAlertAction(title: NSLocalizedString("OK", comment: ""), style: .default, handler: nil)
+            alert.addAction(okAction)
+
+            present(alert, animated: true, completion: nil)
+            return
+        }
+        guard MFMailComposeViewController.canSendMail() else {
+            UIPasteboard.general.string = emailRecipient
+            
+            let title = NSLocalizedString("Your device can not send email", comment: "Your device can not send email")
+            let message = String.localizedStringWithFormat("The email address %@ of %@ has been copied to the pasteboard.", emailRecipient, displayName)
+            
+            let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+            
+            let okAction = UIAlertAction(title: NSLocalizedString("OK", comment: ""), style: .default, handler: nil)
+            alert.addAction(okAction)
+            present(alert, animated: true, completion: nil)
+
+            return
+        }
+        
+        let body = String.localizedStringWithFormat("Dear %@,\n\n", displayName)
+        
+        let mc = MFMailComposeViewController()
+        mc.mailComposeDelegate = self
+        mc.setToRecipients([emailRecipient])
+        mc.setMessageBody(body, isHTML: false)
+        mc.setSubject("Test")
+        //The navigation tint color is not propagated by UIAppearance for some reason. So setting it manually.
+        mc.navigationBar.tintColor = .white
+        present(mc, animated: true, completion: nil)
+
+    }
+    
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        switch result {
+        case .cancelled:
+            print("Mail cancelled")
+        case .saved:
+            print("Mail saved")
+        case .sent:
+            print("Mail sent")
+        case .failed:
+            print("Mail sent failure: \(error?.localizedDescription)")
+        }
+        
+        self.dismiss(animated: true, completion: nil)
     }
 }
 
